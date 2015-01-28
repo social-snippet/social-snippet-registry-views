ca73b8b0cbdcf1284cc8c3edf4b11d89aab1098c
Hiroyuki Sano <sh19910711@gmail.com>
2015-01-28 11:44:10 +0900

    common_views: use slim include plugin

--- lib/social_snippet/registry_views/common_views.rb
+++ lib/social_snippet/registry_views/common_views.rb
@@ -1,1 +1,6 @@
 class SocialSnippet::RegistryViews::CommonViews < Padrino::Application; end
+
+require "slim/include"
+Slim::Engine.set_options :include_dirs => [
+  SocialSnippet::RegistryViews::CommonViews.view_path("templates"),
+]
