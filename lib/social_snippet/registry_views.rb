cbe23e5a915d57f85e5c626c5b63fd65bf149d73
Hiroyuki Sano <sh19910711@gmail.com>
2015-01-28 10:16:37 +0900

    registry_views: fix module path

--- lib/social_snippet/registry_views.rb
+++ lib/social_snippet/registry_views.rb
@@ -1,3 +1,6 @@
-module SocialSnippet::RegistryViews; end
+module SocialSnippet
+  module RegistryViews; end
+end
+
 require_relative "registry_views/version"
 require_relative "registry_views/common_views"
